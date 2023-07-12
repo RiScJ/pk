7c35970cf653972434527c3853fbe8b6cda598cf
Riley Scott Jacob <rilscoja@gmail.com>
2023-07-11 22:23:34 -0400

    Write doxygen for pk header

--- pk.h
+++ pk.h
@@ -1,3 +1,9 @@
+/**
+ * @file pk.h
+ * @author Riley Scott Jacob
+ * @brief Function declarations used by PKC and PKD
+ *
+ */
 #ifndef PK_H
 #define PK_H
 
@@ -20,7 +26,25 @@
 #include "pk_err.h"
 #include "pk_defs.h"
 
+/**
+ * @brief Reads encryption key in from configuration file
+ *
+ * Loads the encryption key stored in the configuration file located
+ * at PK_FP_KEY.
+ *
+ * @param[out] key Key read in from the configuration file
+ */
 int read_keyfile(unsigned char* key);
+
+/**
+ * @brief Reads port sequence to knock from configuration file
+ * 
+ * Loads the sequence of ports to knock in from the configuration file
+ * located at PK_FP_PORTS.
+ *
+ * @param[out] ports Sequence of ports to knock
+ * @param[out] portc Number of ports to knock
+ */
 int read_portfile(unsigned short* ports, int* portc);
 
 #endif // PK_H
